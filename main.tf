5a0c8d9496bc9bb4db6f635710e7bb9af1ad0b3e
Celeste Vazquez <0395132@student.cabrillo.edu>
2025-11-25 02:07:35 +0000

    Add logging

--- main.tf
+++ main.tf
@@ -17,30 +17,123 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_service_account" "vm_sa" {
+  account_id = "vm-sa-wiki"
+  display_name = "VM Service Account" 
+}
+
+resource "google_project_iam_member" "vm_sa_monitoring_writer" {
+  project = var.project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_project_iam_member" "vm_sa_logging_writer" {
+  project = var.project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_compute_disk" "persistent_disk" {
+  name = "vm-persistent-disk" 
+  type = "pd-balanced"
+  size = 10
+  zone = var.zone
+}
+
+resource "google_compute_instance" "db_instance" {
+  name         = "db-instance"
   machine_type = "e2-small"
-  tags         = ["web", "dev"]
+  tags         = ["db"]
   allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      # image = "debian-cloud/debian-12"
-      image = "cos-cloud/cos-stable"
+      image = "debian-cloud/debian-12"
     }
   }
 
+  attached_disk {
+    source = google_compute_disk.persistent_disk.id
+    device_name = "persistent-disk-1"
+  }
+  
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
     }
   }
+
+  service_account {
+    email = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
 }
 
-output "ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+resource "google_compute_instance" "web_instance" {
+  name         = "web-instance"
+  machine_type = "e2-small"
+  tags         = ["web"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+  
+   service_account {
+    email = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name         = "terraform-network-allow-http"
+  network      = google_compute_network.vpc_network.name
+  allow{
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
+}
+
+resource "google_compute_firewall" "allow_db" {
+  name         = "terraform-network-allow-db"
+  network      = google_compute_network.vpc_network.name
+  allow{
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+  source_tags = ["web"]
+  target_tags   = ["db"]
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name         = "terraform-network-allow-ssh"
+  network      = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "db-ip" {
+  value = google_compute_instance.db_instance.network_interface.0.network_ip
+}
+
+output "web-ip" {
+  value = google_compute_instance.web_instance.network_interface.0.network_ip
 }
 
 output "external_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.web_instance.network_interface.0.access_config.0.nat_ip
 }
